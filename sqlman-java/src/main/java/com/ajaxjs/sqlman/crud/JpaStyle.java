a8c692e593ce34d8adf047ca4bb7b772e2ce2754
sp42 <sp42@qq.com>
2025-03-29 23:05:29 +0800

    ci

---
+++ sqlman-java/src/main/java/com/ajaxjs/sqlman/crud/JpaStyle.java
@@ -0,0 +1,6 @@
+package com.ajaxjs.sqlman;
+
+import java.io.Serializable;
+
+public abstract interface JpaStyle<T, ID extends Serializable> {
+}
---
+++ sqlman-java/src/test/java/com/ajaxjs/sqlman/crud/TestJpaStyle.java
@@ -0,0 +1,2 @@
+package com.ajaxjs.sqlman.crud;public class TestJpaStyle {
+}
